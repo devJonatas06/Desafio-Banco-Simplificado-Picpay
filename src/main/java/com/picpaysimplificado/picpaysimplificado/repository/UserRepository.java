4a6104883acddc44a6430901c00965856f9bddf2
JonatasFreitas <jonatasaple@gmail.com>
2025-08-05 15:31:50 -0300

    Corrigindo User repository

--- src/main/java/com/picpaysimplificado/picpaysimplificado/repository/UserRepository.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByDocument(String cpf);
+    Optional<User> findUserByCpf(String cpf);
     Optional<User> findUserById(long id);
     Optional<User> findUserByEmail(String email);
 
